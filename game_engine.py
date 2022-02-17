f8293b6dbc1c6b2d8d8f6cc8c2581e5a592be48b
PatrickDuong3001 <baophuc2000@gmail.com>
2022-02-16 23:44:01 -0500

    fix bug in detecting key mouse pressed. Also, add a file to generate number of applicants and calculate the evil meter based on recruiter's choice

---
+++ game_engine.py
@@ -0,0 +1,87 @@
+from random import randint
+
+class num_applicant_generator(object):
+    #evil score is 0 initially, but will be calculated and returned differently
+    def __init__(self,num_skills,num_exp,pay_amount,job_type,evil_score):   
+        super().__init__()
+        self.skills = num_skills
+        self.exp = num_exp
+        self.pay = pay_amount
+        self.job = job_type
+        self.evil = evil_score
+    
+    def return_num_of_applicants(self):   
+        #comprehensively set number of applicants
+        #in mainGame, call this before evil_result()
+        if self.job == 0:    #internship
+            if self.pay == 0:     #unpaid. 
+                if self.exp < 1:
+                    self.evil += 5
+                    return randint(20,40)
+                elif self.exp >= 1 and self.exp < 4: 
+                    self.evil += 6
+                    return randint(0,5)
+                elif self.exp >= 4: 
+                    self.evil += 7
+                    return 0
+            elif self.pay == 1:   #low-ball pay. evil + 3
+                self.evil += 3
+                if self.exp < 1:
+                    self.evil += 3
+                    return randint(41,60)
+                elif self.exp >= 1 and self.exp < 4: 
+                    self.evil += 4
+                    return randint(10,20)
+                elif self.exp >= 4: 
+                    self.evil += 5
+                    return randint(0,5)   #why some people still apply? lol
+            elif self.pay == 2:   #good pay. evil - 3
+                if self.exp < 1: 
+                    self.evil -= 3
+                    return randint(61,100)
+                elif self.exp >= 1 and self.exp < 4:
+                    self.evil -= 2
+                    return randint(20,30)
+                elif self.exp >= 4:
+                    self.evil -= 1
+                    return randint(5,10)
+        elif self.job == 1:   #mid-level job
+            if self.pay == 0:     #unpaid. level + 8
+                self.evil += 8
+                return 0       #obviously
+            elif self.pay == 1:   #low-ball pay. level + 5
+                if self.exp < 4: 
+                    self.evil += 5
+                    return randint(6,15)
+                elif self.exp >= 4: 
+                    self.evil += 6
+                    return randint(0,5)   
+            elif self.pay == 2:   #good pay. level - 4
+                if self.exp < 4:
+                    self.evil -= 4
+                    return randint(51,100)
+                elif self.exp >= 4:
+                    self.evil -= 3
+                    return randint(40,50)
+        else:                   #senior-level job
+            if self.pay == 0:     #unpaid. level + 12
+                self.evil += 12
+                return 0       #obviously
+            elif self.pay == 1:   #low-ball pay. level + 8
+                self.evil += 8
+                return 0  
+            elif self.pay == 2:   #good pay. level - 1
+                if self.exp < 4:
+                    self.evil -= 2 
+                    return randint(40,60)
+                elif self.exp >= 4:
+                    self.evil -= 1
+                    return randint(15,30)
+                    
+    def evil_result(self):
+        return self.evil          
+    
+            
+            
+        
+             
--- mainGame.py
+++ mainGame.py
@@ -6,6 +6,13 @@ WIDTH = 900
 HEIGHT = 500
 pygame.init()
 
+#game variables initiate
+budget = 0
+years_of_experience = 0
+pay_type = [0,1,2,3]      #will influence number of job apps
+job_type = [0,1,2]
+evil_score = 0
+
 #screen window setup
 pygame.display.set_caption('Become a Recruiter')  #set the title for the game
 screen = pygame.display.set_mode((WIDTH,HEIGHT)) #create the game screen
@@ -21,7 +28,7 @@ start_button = pygame.image.load("data/start_button.png").convert_alpha()
 guide_button = pygame.image.load("data/guide_button.png").convert_alpha()
 setting_button = pygame.image.load("data/setting_button.png").convert_alpha()
 info = pygame.font.Font("data/arial.TTF",15)
-name = info.render("by Patrick",False,"White")
+name = info.render("Patrick & Quan",False,"White")
 title_start = pygame.font.Font("data/arial.TTF",50)
 title = title_start.render("Recruiter Simulator",False,"#48dcff")
 
@@ -32,16 +39,16 @@ title = title_start.render("Recruiter Simulator",False,"#48dcff")
 game_activate = False
 game_run = True
 
-while game_run:    #game_loop
-    pos = pygame.mouse.get_pos()
-    
+while game_run:    #game_loop    
     while start_screen == True:
+        pos = pygame.mouse.get_pos()
+
         screen.blit(start_image,(0,0))
         start_button_rect = screen.blit(start_button,(300,80))
         guide_button_rect = screen.blit(guide_button,(450,80))
         setting_button_rect = screen.blit(setting_button,(850,450))
         
-        screen.blit(name,(320,470))
+        screen.blit(name,(310,470))
         screen.blit(title,(230,0))
         
         pygame.display.update()
@@ -56,12 +63,13 @@ while game_run:    #game_loop
                     start_screen = False
                     game_activate = True
                 elif guide_button_rect.collidepoint(pos):   #click in Guide button
+                    print("Hello")
                     pygame.quit()    #quit game
                     sys.exit()
                 elif setting_button_rect.collidepoint(pos):
+                    print("Hello")
                     pygame.quit()    #quit game
                     sys.exit()
-    
-    while game_activate == True:
-        pass
+    while game_activate:
+        pos = pygame.mouse.get_pos()
 
