2b6397d5f58614a9f848af45555196f338dba3c5
PatrickDuong3001 <baophuc2000@gmail.com>
2022-02-19 18:58:23 -0500

    add username input and modify UI on the company selection page

--- mainGame.py
+++ mainGame.py
@@ -1,10 +1,17 @@
 import pygame
 import os
 import sys
+import configparser
+import argparse
+
+#config = configparser.ConfigParser()
+#config.read("data/game_variables.ini")
 
 WIDTH = 900
 HEIGHT = 500
+FPS = 60
 pygame.init()
+speed = pygame.time.Clock()
 
 #game variables initiate
 budget = 0
@@ -20,6 +27,8 @@ screen = pygame.display.set_mode((WIDTH,HEIGHT)) #create the game screen
 #all songs
 intro_song = pygame.mixer.Sound("data/intro_song.mp3")
 intro_song.set_volume(0.1)
+ingame_song = pygame.mixer.Sound("data/ingame_song.mp3")
+ingame_song.set_volume(0.1)
 
 #start game screen setup
 start_image = pygame.image.load("data/start_screen.png").convert()
@@ -45,6 +54,20 @@ startup_money = pygame.image.load("data/small_stack.png")
 budget_font = pygame.font.Font("data/animation.TTF",16)
 budget_text_bigtech = budget_font.render("Budget: 250,000",False,"White")
 budget_text_startup = budget_font.render("Budget: 85,000",False,"White")
+inputed = False
+user_font = pygame.font.Font("data/arial.TTF",20)
+user_text = ''
+input_window = pygame.Rect(450,423,140,32)
+user_require_font = pygame.font.Font("data/animation.TTF",20)
+user_require = user_require_font.render("Name: ",False,"White")
+message_font = pygame.font.Font("data/arial.TTF",20)
+message = message_font.render("The company only allows you to work after knowing your name!",True,"White")
+
+#ingame screen setup, phase 1
+ingame = False
+startup_thread = False
+bigtech_thread = False
+start_up_environment = pygame.image.load("data/office.png")
 
 
 #control panel
@@ -53,7 +76,6 @@ game_run = True
 
 while game_run:    #game_loop    
     while start_screen == True:
-        pos = pygame.mouse.get_pos()
 
         screen.blit(start_image,(0,0))
         start_button_rect = screen.blit(start_button,(300,80))
@@ -71,35 +93,74 @@ while game_run:    #game_loop
                 pygame.quit()    #quit game
                 sys.exit()
             if event.type  == pygame.MOUSEBUTTONDOWN:       #all click event happens here
-                if start_button_rect.collidepoint(pos):     #click on Start button
+                if start_button_rect.collidepoint(event.pos):     #click on Start button
                     start_screen = False
                     game_activate = True
-                elif guide_button_rect.collidepoint(pos):   #click in Guide button
+                elif guide_button_rect.collidepoint(event.pos):   #click in Guide button
                     print("Hello")
                     pygame.quit()    #quit game
                     sys.exit()
-                elif setting_button_rect.collidepoint(pos):
+                elif setting_button_rect.collidepoint(event.pos):
                     print("Hello")
                     pygame.quit()    #quit game
                     sys.exit()
-    while game_activate:
-        pos = pygame.mouse.get_pos()
-        
+    while game_activate:        
         screen.fill("#48dcff")
-        startup_rect= screen.blit(startup_image,(175,185))
-        bigtech_rect = screen.blit(big_tect_image,(475,185))
-        screen.blit(title_mode_screen,(65,50))
-        screen.blit(startup_text,(220,140))
-        screen.blit(bigtech_text,(520,140))
-        screen.blit(bigtech_money,(675,380))
-        screen.blit(budget_text_bigtech,(480,400))
-        screen.blit(budget_text_startup,(190,400))
-        screen.blit(startup_money,(370,390))
+        startup_rect= screen.blit(startup_image,(175,165))
+        bigtech_rect = screen.blit(big_tect_image,(475,165))
+        pygame.draw.rect(screen,pygame.Color("#ADD8E6"),input_window)
+        screen.blit(user_require,(360,430))
+        screen.blit(title_mode_screen,(65,40))
+        screen.blit(startup_text,(220,120))
+        screen.blit(bigtech_text,(520,120))
+        screen.blit(bigtech_money,(675,360))
+        screen.blit(budget_text_bigtech,(480,380))
+        screen.blit(budget_text_startup,(190,380))
+        screen.blit(startup_money,(370,370))
+        screen.blit(message,(170,475))
         pygame.display.update()
-
-        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()    #quit game
-                sys.exit()
-
+                sys.exit()                
+            if event.type  == pygame.MOUSEBUTTONDOWN:       #all click event happens here
+                inputed = False
+                if startup_rect.collidepoint(event.pos):     #click on Start button
+                    if len(user_text) != 0:
+                        game_activate = False
+                        ingame = True
+                        startup_thread = True
+                elif bigtech_rect.collidepoint(event.pos):   #click in Guide button
+                    if len(user_text) == 0:
+                        pass
+                    else:  
+                        game_activate = False
+                        ingame = True
+                        bigtech_thread = True
+                elif input_window.collidepoint(event.pos):
+                    inputed = True
+                    
+            if event.type == pygame.KEYDOWN:
+                if inputed == True:
+                    if event.key == pygame.K_BACKSPACE:
+                        user_text = user_text[:-1]
+                    else:
+                        user_text += event.unicode
+                    if len(user_text) == 13:
+                        inputed = False
+        pygame.draw.rect(screen,pygame.Color("#ADD8E6"),input_window)
+        text_surface = user_font.render(user_text,True,(255,255,255))
+        input_window.w = max(100,text_surface.get_width()+5)
+        screen.blit(text_surface,(input_window.x + 5, input_window.y + 5))
+        pygame.display.update(input_window)
+                        
+    while ingame:
+        intro_song.stop()
+        ingame_song.play(-1)        
+        
+        if startup_thread == True: 
+            screen.blit(start_up_environment,(0,0))
+            pygame.display.update()
+            
+    speed.tick(FPS)
+            
